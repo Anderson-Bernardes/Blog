81d45067a41e5970059793b87fdd71b17bf44bba
Anderson-Bernardes <anderson_megax@hotmail.com>
2019-09-30 20:41:51 -0300

    #1: Manutenção de posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def preenche_titulo(titulo_de_pagina='')
+    titulo_padrao = "Blog App"
+    if titulo_de_pagina.empty?
+      titulo_padrao
+    else
+      "#{titulo_de_pagina} | #{titulo_padrao}"
+    end
+  end
 end
